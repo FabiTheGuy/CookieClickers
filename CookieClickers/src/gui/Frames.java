db44e6fba7ad88060e09b6ebf41c01a4dfe4ce1c
fabiandoller <fabian.doeller@gmail.com>
2020-02-04 08:57:33 +0100

    changed Controls and update Frames

--- CookieClickers/src/gui/Frames.java
+++ CookieClickers/src/gui/Frames.java
@@ -1,10 +1,17 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import controls.Controls;
 
 public class Frames {
 
+	Controls controls = new Controls();
+	
 	public JPanel MenuFrame() {
 		JPanel MenuPanel = new JPanel();
 		JButton startGameButton = new JButton();
@@ -17,13 +24,38 @@ public class Frames {
 		exitButton.setBounds(0, 0, 0, 0);
 		
 		MenuPanel.add(startGameButton);
+		MenuPanel.add(settingsButton);
+		MenuPanel.add(exitButton);
 		return (MenuPanel);
 	}
 	
 	public JPanel SettingsFrame() {
 		JPanel SettingsPanel = new JPanel();
+		JTextField WidthTextField = new JTextField();
+		JTextField HeightTextField = new JTextField();
+		JButton ResizableButtonTrue = new JButton();
+		JButton ResizableButtonFalse = new JButton();
+		JButton ApplySettingsButton = new JButton();
 		SettingsPanel.setVisible(Settings.SettingsFrameVisibility);
 		
+		WidthTextField.setBounds(0, 0, 0, 0);
+		HeightTextField.setBounds(0, 0, 0, 0);
+		ResizableButtonTrue.setBounds(0, 0, 0, 0);
+		ResizableButtonFalse.setBounds(0, 0, 0, 0);
+		ApplySettingsButton.setBounds(0, 0, 0, 0);
+		
+		ResizableButtonTrue.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				controls.ControlsSettingsFrameSetResizableTrue();
+			}
+		});
+		
+		SettingsPanel.add(WidthTextField);
+		SettingsPanel.add(HeightTextField);
+		SettingsPanel.add(ResizableButtonTrue);
+		SettingsPanel.add(ResizableButtonFalse);
+		SettingsPanel.add(ApplySettingsButton);
 		return (SettingsPanel);
 	}
 	
